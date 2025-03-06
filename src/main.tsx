1af0295831cbcafc464799708732d4ffd3cc17cf
eduzayn <ti@eduzayn.com.br>
2025-03-06 08:20:44 +0000

    Fix: configuração de rotas e redirecionamentos

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
 
 // Definir Button globalmente para evitar erros de referência
 window.Button = window.Button || {};
@@ -17,9 +16,7 @@ const basename = import.meta.env.VITE_BASE_PATH || "/";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
