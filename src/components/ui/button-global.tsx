142315b5eb7dc28eacc5c6793caa294507e8ee7d
eduzayn <ti@eduzayn.com.br>
2025-03-07 01:42:58 +0000

    https://seu-projeto.vercel.app

--- src/components/ui/button-global.tsx
+++ src/components/ui/button-global.tsx
@@ -53,9 +53,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = "Button";
 
-// Definir o componente Button globalmente
-if (typeof window !== "undefined") {
-  window.Button = Button;
-}
-
 export { Button, buttonVariants };
